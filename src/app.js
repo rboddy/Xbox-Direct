fb5d10cde5b82367fd041514bc896ad828d053bb
Ryan <avidryanb@gmail.com>
2017-08-30 14:15:29 -0400

    Stylized the buttons and wrote some functions tied to dom events for some neat UI functionality

--- src/app.js
+++ src/app.js
@@ -2,4 +2,24 @@ export class App {
   constructor() {
     this.message = 'Welcome to Xbox Direct';
   }
+
+  findGamerTag() {
+    console.log('Working');
+  }
+
+  clearInput() {
+    document.querySelector('#gamerTag').value = '';
+    document.querySelector('#clearBtn').style.display = "none";
+  }
+  showClear() {
+    var inputText = document.querySelector('#gamerTag').value;
+    var clearBtn = document.querySelector('#clearBtn');
+
+    if(inputText.length > 0){
+      clearBtn.style.display = "inline";
+    }
+    else {
+      clearBtn.style.display = "none";
+    }
+  }
 }
